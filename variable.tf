66c62c370b256232755b22c5b53745ac2f257a9d
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-09-14 12:49:49 +0700

    Revert "fail"
    This reverts commit a3bace55822f09194fce0c4a897040023e83153a.

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "vpc_name" {
-  default = "tuantranvpc"
+  default = "tuantran-tf-vpc"
 }
 
 variable "cidrvpc" {
@@ -8,37 +8,11 @@ variable "cidrvpc" {
 
 variable "tags" {
   default = {
-    Name  = "tuantran-vpc"
+    Name = "tuantran-vpc"
     Owner = "tuantran"
   }
 }
 
-variable "bastion_definition" {
-  description = "The definition of bastion instance"
-  # type = map(object({
-  #   bastion_name                = string
-  #   bastion_public_key          = string
-  #   bastion_ami                 = string
-  #   bastion_instance_class      = string
-  #   trusted_ips                 = set(string)
-  #   user_data_base64            = string
-  #   associate_public_ip_address = bool
-  #   bastion_monitoring          = bool
-  #   ext-tags  = map(object({}))
-  # }))
-  default = {
-    "bastion" = {
-      associate_public_ip_address = false
-      bastion_ami                 = "ami-0023481579962abd4"
-      bastion_instance_class      = "t3.small"
-      bastion_monitoring          = true
-      bastion_name                = "bastion"
-      bastion_public_key          = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyqbDZMKTXSTCM9Sf6k2Oxg/yU+xPESrtdMManPkQjp tranvuonganhtuan@gmail.com"
-      trusted_ips                 = ["115.78.133.246/32"]
-      user_data_base64            = null
-      ext-tags = {
-        "fucnt" = "demo-tf"
-      }
-    }
-  }
+variable "az_counts" {
+  default = 3
 }
