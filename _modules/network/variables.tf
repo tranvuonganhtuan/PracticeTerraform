7ac40e0fe8330fdb6cd55aa14c9c036e084fdf97
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-09-15 08:47:32 +0700

    test4

---
+++ _modules/network/variables.tf
@@ -0,0 +1,15 @@
+variable "azs" {
+
+}
+variable "vpc_name" {
+
+}
+variable "tags" {
+
+}
+variable "cidrvpc" {
+
+}
+variable "azname" {
+
+}
--- variable.tf
+++ variable.tf
@@ -13,6 +13,53 @@ variable "tags" {
   }
 }
 
-variable "az_counts" {
-  default = 3
+# variable "az_counts" {
+#   default = 3
+# }
+
+variable "vm-config" {
+  default = {
+    vm1 = {
+      instance_type = "t3.small",
+      tags = {
+        "ext-name" = "vm2"
+        "funct"    = "purpose test"
+      }
+    },
+    vm2 = {
+      instance_type = "t3.medium",
+      tags          = {}
+    }
+  }
+
+}
+
+variable "bastion_definition" {
+  description = "The definition of bastion instance"
+  # type = map(object({
+  #   bastion_name                = string
+  #   bastion_public_key          = string
+  #   bastion_ami                 = string
+  #   bastion_instance_class      = string
+  #   trusted_ips                 = set(string)
+  #   user_data_base64            = string
+  #   associate_public_ip_address = bool
+  #   bastion_monitoring          = bool
+  #   ext-tags  = map(object({}))
+  # }))
+  default = {
+    "bastion" = {
+      associate_public_ip_address = false
+      bastion_ami                 = "ami-0023481579962abd4"
+      bastion_instance_class      = "t3.small"
+      bastion_monitoring          = true
+      bastion_name                = "bastion"
+      bastion_public_key          = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJyqbDZMKTXSTCM9Sf6k2Oxg/yU+xPESrtdMManPkQjp tranvuonganhtuan@gmail.com"
+      trusted_ips                 = ["42.112.79.203/32"]
+      user_data_base64            = null
+      ext-tags = {
+        "fucnt" = "demo-tf"
+      }
+    }
+  }
 }
