61c8fb083f781796957a570daa969c56ba4cd1a1
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-09-24 08:48:54 +0700

    gafana

---
+++ _modules/eks/output.tf
@@ -0,0 +1,9 @@
+output "cluster_id" {
+  value = module.eks.cluster_id
+}
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+output "cluster_oidc_issuer_url" {
+  value = module.eks.cluster_oidc_issuer_url
+}
--- _modules/s3/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "tuantran-bucket"
-  #   acl    = "private"
-  
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
