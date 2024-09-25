89aff555c13fff7c5cdcce2055bcc3f14fd2bc87
AnhTuanTran <tranvuonganhtuan@gmail.com>
2024-09-25 13:12:26 +0700

    config

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "eks" {
   eks_cluser_enginee_version = "1.30"
   vpc_id                     = module.vpc.vpc_id
   private_subnet_ids         = module.vpc.private_subnet_id
-  instance_types             = ["t3.large", "t3.large", "t3.medium", "t3.medium"]
+  instance_types             = ["t2.large", "t3.large", "t2.medium", "t3.medium"]
   ami_id                     = "ami-0023481579962abd4"
   tags                       = var.tags
 }
